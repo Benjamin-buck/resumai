e806d1e1d0775111837a3ae7867b4dbc0bdf8693
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2024-08-31 18:37:23 -0400

    adds dashboard page

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -1,34 +1,41 @@
+import { plans } from "@/data";
 import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
+interface Props {
+    id: number;
+    label: string;
+    description: string,
+    features: [];
+}
 
-const PlanCard = () => {
+const PlanCard = ({id, label, features, description}: Props) => {
     return (
-        <div className='flex basis-1/4 border border-1 border-[#e3e3e3] rounded-lg flex-col mb-10'>
-            <div className='mx-3 my-3 bg-purple px-3 py-1 rounded-lg text-white w-[3rem] flex justify-center'>
-                Pro
-            </div>
+        <div key={id} className='flex basis-1/4 border border-1 border-[#e3e3e3] rounded-lg flex-col mb-10 h-[600px] justify-between'>
+            <div>
+                <div className='mx-3 my-3 bg-purple px-3 py-1 rounded-lg text-white w-[4.5rem] flex justify-center'>
+                    {label}
+                </div>
             <div className='mx-3'>
-                <p className='text-gray'>This plan is perfect for the active job hunter who is looking to get placed in a job in 3-6 weeks.</p>
+                <p className='text-gray h-[100px]'>{description}</p>
                 <div className='flex items-center py-10'>
                     <h3 className='text-[30px] font-semibold'>$299</h3>
                     <span className='text-gray'>/month</span>
                 </div>
                 <div className="border-y-[1px] border-[#dedede]">
                     <ul className="py-5">
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 1</li>
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 2</li>
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 3</li>
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 4</li>
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 5</li>
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 6</li>
-                        <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> Feature 7</li>
+                        {features.map((feature) => (
+                            <li className="flex gap-2 items-center text-gray px-2 py-1"><IoIosCheckmarkCircleOutline /> {feature}</li>
+                        ))} 
                     </ul>
                 </div>
-                <div>
-                    <button className="border border-[#dedede] w-full my-5 py-2 rounded-lg">
-                        Get Started
-                    </button>
-                </div>
+                
+            </div>
+            </div>
+            
+            <div className="mx-3">
+                <button className="border border-[#dedede] w-full my-5 py-2 rounded-lg">
+                    Get Started
+                </button>
             </div>
             
         </div>
@@ -37,15 +44,16 @@ const PlanCard = () => {
  
 const Plans = () => {
   return (
-    <div className='md:mt-[100px] mt-[50px]'> 
+    <div id="pricing" className='md:mt-[100px] mt-[50px]'> 
         <div className='flex flex-col justify-center items-center md:px-0 px-5 text-center '>
             <h2 className='md:text-[40px] text-[30px] font-semibold text-center'>Choose your right plan!</h2>
             <p className="">Select from the best plans, ensuring your perfect needs.</p>
         </div>
         <div className='flex flex-wrap max-md:flex-col mx-5 flex-row justify-center gap-5 mt-10'>
-            <PlanCard />
-            <PlanCard />
-            <PlanCard />
+            {plans.map(({id, label, features, description}) => (
+                <PlanCard id={id} label={label} features={features} description={description}/>
+            ))}
+            
         </div>
     </div>
   )
--- data/index.ts
+++ data/index.ts
@@ -1,5 +1,40 @@
 export const navLinks = [
     {id: 1, label: 'Demo', href: '/pricing'},
-    {id: 1, label: 'Pricing', href: '/pricing'},
+    {id: 1, label: 'Pricing', href: '#pricing'},
     {id: 1, label: 'FAQ', href: '/pricing'},
-]
+];
+
+export const plans = [
+    {
+        id: 1, 
+        label: 'Pro',
+        description: 'This package is perfect for the weekend job hunter, who is applying on a few jobs per week and would like to maximize those results.',
+        features: [
+            'Resume builder',
+            '400 credits',
+            'AI generated cover leters'
+        ],
+    },
+    {
+        id: 1, 
+        label: 'Custom',
+        description: 'This package is perfect for the active job hunter, who is applying for multiple jobs daily. This package maximizes your potential of getting hired.',
+        features: [
+            'Resume builder',
+            '400 credits',
+            'AI generated cover leters',
+            'Customizable resume templates',
+            'Job references'
+        ]
+    },
+    {
+        id: 1, 
+        label: 'PAYG',
+        description: 'This package is perfect for the casual job hunter, who applies on jobs occasionally. This package is a "Pay as you go" option that allows you to pay for what you use.',
+        features: [
+            'Resume builder',
+            '400 credits',
+            'AI generated cover leters'
+        ]
+    },
+];
